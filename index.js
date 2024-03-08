54ab0ac08dba867bf062fdee63c2e7e18506576b
youkyoung546 <32660321-youkyoung546@users.noreply.replit.com>
2024-03-08 02:54:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function fizzbuzz(n){
+  if(n%3 == 0 && n%5 == 0) {
+    console.log('fizzbuzz');
+  }
+  else if(n%3 == 0){
+    console.log('fizz');
+  }
+  else if(n%5 == 0){
+    console.log('buzz');
+  }
+  else{
+    console.log(n);
+  }
+  
+}
+
+for(let i = 1; i <= 100; i++){
+  fizzbuzz(i);
+}
